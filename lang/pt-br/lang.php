cf413b93c506e4ee50a6ffc145b5431007d002ba
Adrian Lang <mail@adrianlang.de>
2010-10-08 19:06:41 +0200

    Add pt-br and ru translations from plugin page

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['openid_login_fieldset']    = 'Login OpenID';
+$lang['openid_account_fieldset']  = 'Conta OpenID';
+
+$lang['openid_url_label'] = 'Endereço da OpenID';
+
+$lang['add_button']             = 'Adicionar';
+$lang['complete_button']        = 'Completar';
+$lang['delete_selected_button'] = 'Apagar selecionado';
+
+$lang['login_link']   = 'Você também pode <a href="%s" class="openid">entrar com sua OpenID</a>.';
+$lang['manage_link']  = 'Você também pode <a href="%s">gerenciar suas identidades OpenID</a>';
+
+$lang['add_openid_title']         = 'Adicionar uma OpenID';
+$lang['openid_identities_title']  = 'Suas identidades OpenID';
+
+$lang['complete_registration_notice']  = 'Você está conectado temporariamente usando sua OpenID. <a href="%s">Termine seu registro</a> para ser um usuário permanente.';
+
+$lang['openid_already_user_error']  = 'Essa identidade OpenID já está sendo usada';
+$lang['enter_valid_openid_error']   = 'Entre com um identificador OpenID válido.';
+
+$lang['openid_identity_added']          = 'Identidade OpenID adicionada com sucesso';
+$lang['openid_authentication_failed']   = 'A autenticação OpenID falhou';
+$lang['openid_authentication_canceled'] = 'Autenticação OpenID cancelada.';
+
+$lang['openid_complete_text'] = 'Por favor verifique e/ou complete as informações da sua conta para terminar o registro.';
+$lang['openid_complete_disabled_text'] = 'Você está corretamente autenticado, mas registros não são possíveis nesta wiki. <a href="%s">Continue aqui.</a>';
+
+$lang['none'] = 'Nenhum.';
---
+++ lang/ru/lang.php
@@ -0,0 +1,30 @@
+<?php
+
+$lang['openid_login_fieldset']    = 'Войти через OpenID';
+$lang['openid_account_fieldset']  = 'Учётная запись OpenID';
+
+$lang['openid_url_label'] = 'Ваш OpenID';
+
+$lang['add_button']             = 'Добавить';
+$lang['complete_button']        = 'Заполнить';
+$lang['delete_selected_button'] = 'Удалить выбранное';
+
+$lang['login_link']   = 'Вы также можете <a href="%s" class="openid">войти с помощью OpenID</a>. Например, если у вас есть Живой Журнал, то у вас уже есть идентификатор OpenID. Он имеет вид <b>myname.livejournal.com</b>';
+$lang['manage_link']  = 'Здесь вы можете <a href="%s">управлять своими учётными записями OpenID identities</a>';
+
+$lang['add_openid_title']         = 'Добавить OpenID';
+$lang['openid_identities_title']  = 'Ваши учётные записи OpenID';
+
+$lang['complete_registration_notice']  = 'Вы вошли, используя временную сессию OpenID. <a href="%s">Завершите регистрацию</a> чтобы сделать её постоянной.';
+
+$lang['openid_already_user_error']  = 'Такая учётная запись OpenID уже используется.';
+$lang['enter_valid_openid_error']   = 'Введите "хорошую, годную" учётную запись OpenID.';
+
+$lang['openid_identity_added']          = 'Успешно добавили учётную запись OpenID.';
+$lang['openid_authentication_failed']   = 'Авторизация с помощью OpenID не удалась.';
+$lang['openid_authentication_canceled'] = 'Авторизация с помощью OpenID была прервана.';
+
+$lang['openid_complete_text'] = 'Теперь можно заполнить краткую анкету. Укажите хотя бы, как к Вам обращаться...';
+$lang['openid_complete_disabled_text'] = 'С одной стороны, всё прошло хорошо, но, с другой стороны, в доступе Вам отказано. <a href="%s">Кликните здесь.</a>';
+
+$lang['none'] = 'Упс!';
